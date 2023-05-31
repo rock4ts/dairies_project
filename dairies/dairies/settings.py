969f62e950e566fb91d1900af41a8dbf1ff25a44
Artyom Sukhov <rocket.sukhov@gmail.com>
2023-06-01 00:33:19 +0300

    change settings and readme

--- dairies/dairies/settings.py
+++ dairies/dairies/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = True  # os.getenv('DEBUG') == 'True'
+DEBUG = os.getenv('DEBUG') == True
 
 ALLOWED_HOSTS = [
     '158.160.8.237',
@@ -79,23 +79,23 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'dairies.wsgi.application'
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
-#         'NAME': os.getenv('DB_NAME', default='postgres'),
-#         'USER': os.getenv('POSTGRES_USER', default='postgres'),
-#         'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='rock4ts@db'),
-#         'HOST': os.getenv('DB_HOST', default='db'),
-#         'PORT': os.getenv('DB_PORT', default=5432)
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'mydatabase',
     }
 }
+
+# DATABASES = {
+# 	'default': {
+# 		'ENGINE': os.getenv('DB_ENGINE'),
+# 		'NAME': os.getenv('DB_NAME'),
+# 		'USER': os.getenv('POSTGRES_USER'),
+# 	        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+# 	        'HOST': os.getenv('DB_HOST'),
+# 	        'PORT': os.getenv('DB_PORT')
+# 	}
+# }
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -122,16 +122,12 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATIC_URL = '/static/'
-# STATIC_URL = '/static/dairies/'
+STATIC_URL = '/static/dairies/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-
-MEDIA_URL = '/media/'
+MEDIA_URL = '/media/dairies/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-# MEDIA_URL = '/media/dairies/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 LOGIN_URL = 'users:login'
 LOGIN_REDIRECT_URL = 'posts:index'
